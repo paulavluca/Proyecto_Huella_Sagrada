a84fb2921352680b84cf21bcc85c699e3e91f906
unknown <paulavluca@gmail.com>
2022-07-29 00:44:06 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//Go top button function
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () =>{
+    if(window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
