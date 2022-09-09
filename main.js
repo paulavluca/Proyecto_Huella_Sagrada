ce568b8fbc17b982d08cc6eac05c439e8578ea71
Paula Lucá <paulavluca@gmail.com>
2022-09-09 01:20:19 -0300

    Commit 9/9

--- main.js
+++ main.js
@@ -96,22 +96,44 @@ if ($(window).scrollTop() > 50) {
 })();
 
 /* ADD REST BUTTON */
-const plus = document.querySelector(".plus"),
-minus = document.querySelector(".minus"),
-num = document.querySelector(".num");
+// const plus = document.querySelector(".plus"),
+// minus = document.querySelector(".minus"),
+// num = document.querySelector(".num");
 
-let a = 0;
+// let a = 0;
 
-plus.addEventListener("click", ()=>{
-    a++;
-    // a = (a < 10) ? "0" + a : a;
-    num.innerText = a;
-    console.log(a);
-});
-minus.addEventListener("click", ()=>{
-    if(a > 0){
-        a--;
-        // a = (a > 1) ? "0" + a : a;
-        num.innerText = a;
-    }
-});
+// plus.addEventListener("click", ()=>{
+//     a++;
+//     // a = (a < 10) ? "0" + a : a;
+//     num.innerText = a;
+//     console.log(a);
+// });
+// minus.addEventListener("click", ()=>{
+//     if(a > 0){
+//         a--;
+//         // a = (a > 1) ? "0" + a : a;
+//         num.innerText = a;
+//     }
+// });
+
+
+/* */
+
+const btnop1 = document.getElementById("op-1");
+const btnop2 = document.getElementById("op-2");
+const op1Text = document.getElementById("op-1-text");
+const op2Text = document.getElementById("op-2-text");
+
+btnop1.onclick = function(){
+    op1Text.style.display = "block";
+    op2Text.style.display = "none";
+    btnop1.style.borderBottom = "8px solid #1f0000";
+    btnop2.style.borderBottom = "none";
+}
+
+btnop2.onclick = function(){
+    op1Text.style.display = "none";
+    op2Text.style.display = "block";
+    btnop1.style.borderBottom = "none";
+    btnop2.style.borderBottom = "8px solid #1f0000";
+}
